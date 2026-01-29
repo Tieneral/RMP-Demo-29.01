1b03ee5b42b03226ac02a29527cb8e0b7d942c3a
Tieneral <shchepinkirill2773@gmail.com>
2026-01-29 14:28:33 +0400

    orders

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            dgv = new DataGridView();
+            panel1 = new Panel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label1 = new Label();
+            button1 = new Button();
+            tabPage2 = new TabPage();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgv).BeginInit();
+            panel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(dgv, 1, 0);
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(965, 382);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dgv
+            // 
+            dgv.BackgroundColor = Color.White;
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv.Dock = DockStyle.Fill;
+            dgv.Location = new Point(203, 3);
+            dgv.Name = "dgv";
+            dgv.Size = new Size(759, 376);
+            dgv.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 224, 192);
+            panel1.Controls.Add(tabControl1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(194, 376);
+            panel1.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(194, 376);
+            tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(186, 348);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Заказы";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(10, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 14);
+            label1.TabIndex = 3;
+            label1.Text = "Посмотреть таблицу заказов";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.Location = new Point(19, 48);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 50);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(186, 416);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(965, 382);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
             Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgv).EndInit();
+            panel1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dgv;
+        private Panel panel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label1;
+        private Button button1;
+        private TabPage tabPage2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Data.SQLite;
+
 namespace RMP_Demo_29._01
 {
     public partial class Form1 : Form
@@ -5,6 +11,29 @@ namespace RMP_Demo_29._01
         public Form1()
         {
             InitializeComponent();
+        }
+        private const string STATUS_IN_PROGRESS = "В процессе";
+        private const string STATUS_COMPLETED = "Завершён";
+
+        public static void OrderView(DataGridView dgv, string dbPath = "..\\..\\..\\!_DB.db")
+        {
+            string connectionString = $"Data Source={dbPath};Version=3;";
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT \r\n    o.ord_id,\r\n    c.name AS client_name,\r\n    o.disc AS description,\r\n    o.start_date,\r\n    o.order_status,\r\n    d.type AS device_type,\r\n    e.name AS employee_name\r\nFROM Orders o\r\nJOIN Clients c ON o.cl_id = c.cl_id\r\nJOIN Devices d ON o.dev_id = d.dev_id\r\nJOIN Employees e ON o.emp_id = e.emp_id\r\nORDER BY o.ord_id;";
+                using (var adapter = new SQLiteDataAdapter(query, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dgv.DataSource = dataTable;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OrderView(dgv);
         }
     }
 }
